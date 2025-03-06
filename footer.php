5a3e7c3aecc83671e60792b191729d5e5febe2e3
David Farinha <davidjosemarreca@gmail.com>
2025-03-06 15:42:07 +0000

    update
    update footer

--- footer.php
+++ footer.php
@@ -3,27 +3,26 @@
     <div class="container footer-top">
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">Mentor</span>
+          <a href="index.php" class="logo d-flex align-items-center">
+            <span class="sitename">Bento de Jesus Caraça</span>
           </a>
           <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
+            <p><a href="https://www.google.com/maps/place/Escola+Profissional+Bento+de+Jesus+Cara%C3%A7a+-+Lisboa/@38.7079433,-9.1447203,17z/data=!3m1!4b1!4m14!1m7!3m6!1s0xd193587275608c1:0x28cf86cb9a4d47ee!2sEscola+Profissional+Bento+de+Jesus+Cara%C3%A7a+-+Lisboa!8m2!3d38.7079392!4d-9.1401069!16s%2Fg%2F11ldk7jpn0!3m5!1s0xd193587275608c1:0x28cf86cb9a4d47ee!8m2!3d38.7079392!4d-9.1401069!16s%2Fg%2F11ldk7jpn0?entry=ttu&g_ep=EgoyMDI1MDMwNC4wIKXMDSoASAFQAw%3D%3D" target="_blank">Rua Vitor Cordon, N1, R/C </a></p>
+            <p>1200-482 Lisboa</p>
+            <p class="mt-3"><strong>Tel:</strong> <span>213 255 326</span></p>
+            <p><strong>Email:</strong> <span>geral@epbjc.pt</span></p>
           </div>
           <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
+            <a href="https://www.facebook.com/EPBJC" target=”_blank”><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/epbjc_escolaprofissional/" target=”_blank”><i class="bi bi-instagram"></i></a>
+            <a href="https://pt.linkedin.com/school/escola-profissional-bento-de-jesus-cara%C3%A7a/" target=”_blank”><i class="bi bi-linkedin"></i></a>
           </div>
         </div>
 
         <div class="col-lg-2 col-md-3 footer-links">
           <h4>Useful Links</h4>
           <ul>
-            <li><a href="#">Home</a></li>
+            <li><a href="index.php">Início</a></li>
             <li><a href="#">About us</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Terms of service</a></li>
