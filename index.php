0cd9ee5bfa8b369b9b159d5fa06019aed6aa452a
julismo <64311197+julismo@users.noreply.github.com>
2025-03-06 10:35:02 +0000

    Update
    Campo das obras

--- index.php
+++ index.php
@@ -145,8 +145,8 @@
 
           <div class="col-lg-3 col-md-6">
             <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="145" data-purecounter-duration="1" class="purecounter"></span>
-              <p>Volumes Publicados</p>
+              <span data-purecounter-start="0" data-purecounter-end="+30" data-purecounter-duration="1" class="purecounter"></span>
+              <p>Artigos científicos</p>
             </div>
           </div><!-- End Stats Item -->
 
@@ -160,7 +160,7 @@
           <div class="col-lg-3 col-md-6">
             <div class="stats-item text-center w-100 h-100">
               <span data-purecounter-start="0" data-purecounter-end="5" data-purecounter-duration="1" class="purecounter"></span>
-              <p> Revistas/Publicações</p>
+              <p>Documentos preservados</p>
             </div>
           </div><!-- End Stats Item -->
 
