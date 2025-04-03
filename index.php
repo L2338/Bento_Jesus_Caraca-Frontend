468fab1cd8986fc3de6e656ee51ade66e2628615
julismo <64311197+julismo@users.noreply.github.com>
2025-04-03 12:27:17 +0100

    UPDATE

--- index.php
+++ index.php
@@ -123,7 +123,7 @@
 
           <div class="col-lg-3 col-md-6">
             <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="9" data-purecounter-duration="1" class="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" class="purecounter"></span>
               <p>Livros Publicados</p>
             </div>
           </div><!-- End Stats Item -->
